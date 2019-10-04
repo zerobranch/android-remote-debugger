eb7d77a9295884d8e59a511968a6f977ac8f4783
arman <asargsyan@movista.ru>
2019-10-04 13:15:22 +0300

    Реализовал разделение лога если превышает максимальный размер

--- debugger/src/main/java/com/sarproj/remotedebugger/logging/RemoteLog.java
+++ debugger/src/main/java/com/sarproj/remotedebugger/logging/RemoteLog.java
@@ -1,15 +1,21 @@
 package com.sarproj.remotedebugger.logging;
 
+import android.os.Build;
+
 import com.sarproj.remotedebugger.source.local.LogLevel;
 import com.sarproj.remotedebugger.source.managers.ContinuousDataBaseManager;
 import com.sarproj.remotedebugger.source.models.LogModel;
 import com.sarproj.remotedebugger.utils.InternalUtils;
+
+import org.jetbrains.annotations.NotNull;
 
 public final class RemoteLog {
     private static final String DEFAULT_TAG = RemoteLog.class.getSimpleName();
     private final boolean isEnabledDefaultLogging;
     private final ContinuousDataBaseManager continuousDataBaseManager;
     private final Logger logger;
+    private static final int MAX_LOG_LENGTH = 2000;
+    private static final int MAX_TAG_LENGTH = 23;
 
     public RemoteLog(Logger logger, ContinuousDataBaseManager continuousDataBaseManager, boolean isEnabledDefaultLogging) {
         if (logger != null) {
@@ -48,7 +54,32 @@ public final class RemoteLog {
         continuousDataBaseManager.addLog(new LogModel(logLevel.name(), tag, msg));
 
         if (isEnabledDefaultLogging) {
-            logger.log(logLevel.priority(), tag, msg, th);
+            if (logger instanceof DefaultLogger) {
+                if (tag.length() > MAX_TAG_LENGTH && Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+                    tag = tag.substring(0, MAX_TAG_LENGTH);
+                }
+            }
+
+            internalLog(logLevel.priority(), tag, msg, th);
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void internalLog(int priority, String tag, @NotNull String msg, Throwable th) {
+        if (msg.length() < MAX_LOG_LENGTH) {
+            logger.log(priority, tag, msg, th);
+            return;
+        }
+
+        for (int i = 0, length = msg.length(); i < length; i++) {
+            int newline = msg.indexOf('\n', i);
+            newline = newline != -1 ? newline : length;
+            do {
+                int end = Math.min(newline, i + MAX_LOG_LENGTH);
+                String part = msg.substring(i, end);
+                logger.log(priority, tag, part, th);
+                i = end;
+            } while (i < newline);
         }
     }
 }
