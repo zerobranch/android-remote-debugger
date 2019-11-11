be176441013653f694f40b13cb218c7a62fccd64
arman <vulturegray@gmail.com>
2019-11-11 23:33:58 +0300

    migrated to Android X

--- app/src/main/java/com/sarproj/example/MainActivity.java
+++ app/src/main/java/com/sarproj/example/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sarproj.example.db.DBHelper;
 import com.sarproj.remotedebugger.RemoteDebugger;
--- remote-debugger/src/main/java/com/sarproj/remotedebugger/AppNotification.java
+++ remote-debugger/src/main/java/com/sarproj/remotedebugger/AppNotification.java
@@ -6,8 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 class AppNotification {
     private static final String GROUP_KEY = "remote_debugger_group";
