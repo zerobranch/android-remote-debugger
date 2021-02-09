9a62bf599e77b8c475e3510473fc88d1aad8c7b3
arman <vulturegray@gmail.com>
2021-02-09 20:36:42 +0300

    add example

---
+++ example/src/main/java/zerobranch/example/App.java
@@ -0,0 +1,11 @@
+package zerobranch.example;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
